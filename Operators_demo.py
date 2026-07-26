6bbc8737e135c33be3b0d654629d0d0b71e0a85a
bhumikasu145-sys <bhumikasu145@gmail.com>
2026-07-26 11:31:28 +0530

    Create Operators_demo.py

---
+++ Operators_demo.py
@@ -0,0 +1,23 @@
+# SLICING EXAMPLE
+name = "Bhumika"
+print("Original name:", name)
+print("First 3 letters:", name[0:3])
+print("Last 3 letters:", name[-3:])
+
+# COMPARISON OPERATORS
+a = 10
+b = 20
+
+print("\nComparison Operators:")
+print("a == b:", a == b)
+print("a != b:", a != b)
+print("a > b:", a > b)
+print("a < b:", a < b)
+
+# LOGICAL OPERATORS
+x = 5
+
+print("\nLogical Operators:")
+print("x > 2 and x < 10:", x > 2 and x < 10)
+print("x > 10 or x < 10:", x > 10 or x < 10)
+print("not(x > 2):", not(x > 2))
